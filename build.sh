d4b79450d9c054a5c8e059fa07436735cbb4ace5
anhelinakhlus <anhelinakhlus@gmail.com>
2026-05-25 13:49:15 +0300

    Fix: add pip install to build script

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# Выход при любой ошибке
 set -o errexit
+
+# 1. Устанавливаем все зависимости (это критически важно!)
+pip install -r requirements.txt
+
+# 2. Применяем миграции
 python manage.py migrate --noinput
+
+# 3. Собираем статику
 python manage.py collectstatic --noinput
