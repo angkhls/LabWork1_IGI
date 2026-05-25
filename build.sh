577bac06b7486f8ba5e978d70146ac7071cfa20e
anhelinakhlus <anhelinakhlus@gmail.com>
2026-05-25 13:47:00 +0300

    Add build.sh for automatic migrations

--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 set -o errexit
-
-# Установка зависимостей
-pip install -r requirements.txt
-
-# Сбор статики
-python manage.py collectstatic --no-input
-
-# Применение миграций
-python manage.py migrate
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
