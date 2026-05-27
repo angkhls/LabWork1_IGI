db8423bd465c672911d8cde9d0b4747bd4a465b4
anhelinakhlus <anhelinakhlus@gmail.com>
2026-05-27 23:59:24 +0300

    add new lb

--- core/settings.py
+++ core/settings.py
@@ -21,8 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    
+    'cloudinary_storage', 
     'django.contrib.staticfiles',
-    'cloudinary_storage',  # ← добавь
     'cloudinary',          # ← добавь
     'factory',
 ]
